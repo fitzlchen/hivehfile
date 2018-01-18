920d9f69cdc386a29ce1592b1fe27858c19ab72d
fitz <fitz@fitzdeMacBook-Pro.local>
2018-01-18 13:14:39 +0800

    change param

--- src/main/java/cn/jiguang/hivehfile/mapreduce/GenericMapReduce.java
+++ src/main/java/cn/jiguang/hivehfile/mapreduce/GenericMapReduce.java
@@ -100,10 +100,8 @@ public class GenericMapReduce extends Configured implements Tool {
         // 内存使用设置
         configuration.set("yarn.scheduler.minimum-allocation-mb", "1024");
         configuration.set("yarn.scheduler.maximum-allocation-mb", "7168");
-        configuration.set("mapreduce.map.memory.mb", "7168");
-        configuration.set("mapreduce.map.java.opts", "-Xmx7168M");
         configuration.set("mapreduce.reduce.memory.mb", "7168");
-        configuration.set("mapreduce.reduce.java.opts", "-Xmx7168M");
+        configuration.set("mapreduce.reduce.java.opts", "-Xmx6144M");
         // ZooKeeper设置
         configuration.set("hbase.zookeeper.quorum", selfDefinedConfig.getHbaseZookeeperQuorum());
         configuration.set("hbase.zookeeper.property.clientPort", selfDefinedConfig.getHbaseZookeeperPropertyClientPort());
